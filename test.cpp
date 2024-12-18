316f5a914b92fad978df29603948554be8595ff2
Yuvraj Singh Kumpavat <115008153+Euphorus@users.noreply.github.com>
2024-12-18 12:49:05 +0000

    christmas update
    Merry Christmas Ben
    - Yuvraj Kumpavat

--- test.cpp
+++ test.cpp
@@ -1,20 +1,77 @@
-// test.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include <stdlib.h>
+#define _RM(float)RAND_MAX
+#define _R(float)rand()
+#define pc putchar
+#define td typedef
+#define pf printf
+td char _C;
+td int _I;
+td void _V;
+//_C s()
+//{
+//	return ((rand() / _RM(float) > 0.95) ? '.' : ' ');
+//}
+//_V t(_I p, _I z, _I o = 0, _C c)
+//{
+//	for (_I i = 1; i < z; i++, pc('\n'))
+//		for (_I j = 0; i < p * 2; j++, pc((j <= (p - i - o) || j >= (p + i + o)) ? s() : '#'));
+//}
 //
+//_V r(_I p, _I w, _I h)
+//{
+//	for (_I j = 0; j < p * 2; j++, pc((j <= (p - w) || j >= (p + w)) ? ' ' : '#'));
+//}
+//
+//_I main()
+//{
+//	t(16, 4), t(16, 5, 1), t(16, 6, 2), t(16, 2, 3);
+//	pf("\n - MERRY CHRISTMAS FROM BEN - \n");
+//}
 
 #include <iostream>
 
-int main()
+
+char snow()
 {
-    std::cout << "Hello World!\n";
+	if ((rand() / (float)RAND_MAX) > 0.95)
+		return '.';
+	else
+		return ' ';
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void Triangle(int cpos, int size, int offset = 0, char c = '#')
+{
+	for (int i = 1; i < size; i++, putchar('\n'))
+	{
+		for (int j = 0; j < cpos * 2; j++)
+		{
+			if (j <= (cpos - i - offset) || j >= (cpos + i + offset))
+				putchar(snow());
+			else
+				putchar('#');
+		}
+	}
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void Rect(int cpos, int w, int h)
+{
+	for (int line = 0; line < h; line++, putchar('\n'))
+	{
+		for (int j = 0; j < cpos * 2; j++)
+		{
+			if (j <= (cpos - w) || j >= (cpos + w))
+				putchar(' ');
+			else
+				putchar('#');
+		}
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	Triangle(16, 4);
+	Triangle(16, 5, 1);
+	Triangle(16, 6, 2);
+	Rect(16, 2, 3);
+	printf("\n -  MERRY CHRISTMAS FROM BEN - \n");
+}
