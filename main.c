133caf6b6c038de1b676167ef98b2c18d1111491
Yuvraj Singh Kumpavat <115008153+Euphorus@users.noreply.github.com>
2025-04-11 11:06:24 +0100

    got some sick stuff in

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include<stdio.h>
-
+#include <raylib.h>
 
 int main(void)
 {
-	printf("Hello, World!");
+	InitWindow(600, 600, "Raylib - basic window");
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+
+			ClearBackground(RAYWHITE);
+
+			DrawText("Congrats! You created your first window!", 90, 200, 20, RED);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
+
+	return 0;
 }
